3425d0093bb3ecca5027c8f691395d94d425db1c
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-27 03:15:18 +0100

    Create SettingsFaceView.xaml.cs

---
+++ src/DimensionalUI.App/Views/Faces/SettingsFaceView.xaml.cs
@@ -0,0 +1,11 @@
+using Windows.UI.Xaml.Controls;
+
+namespace DimensionalUI.App.Views.Faces;
+
+public sealed partial class SettingsFaceView : Page
+{
+    public SettingsFaceView()
+    {
+        this.InitializeComponent();
+    }
+}
