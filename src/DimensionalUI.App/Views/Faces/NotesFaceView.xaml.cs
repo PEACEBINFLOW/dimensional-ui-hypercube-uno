3c018ac16c8bda2f350665deff4bdd6fc9ee7e84
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-27 03:10:51 +0100

    Create NotesFaceView.xaml.cs

---
+++ src/DimensionalUI.App/Views/Faces/NotesFaceView.xaml.cs
@@ -0,0 +1,11 @@
+using Windows.UI.Xaml.Controls;
+
+namespace DimensionalUI.App.Views.Faces;
+
+public sealed partial class NotesFaceView : Page
+{
+    public NotesFaceView()
+    {
+        this.InitializeComponent();
+    }
+}
