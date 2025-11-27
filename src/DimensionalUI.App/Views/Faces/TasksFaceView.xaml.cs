fa4f2f566bba25716518342320f69007ed934d85
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-27 03:08:29 +0100

    Create TasksFaceView.xaml.cs

---
+++ src/DimensionalUI.App/Views/Faces/TasksFaceView.xaml.cs
@@ -0,0 +1,11 @@
+using Windows.UI.Xaml.Controls;
+
+namespace DimensionalUI.App.Views.Faces;
+
+public sealed partial class TasksFaceView : Page
+{
+    public TasksFaceView()
+    {
+        this.InitializeComponent();
+    }
+}
