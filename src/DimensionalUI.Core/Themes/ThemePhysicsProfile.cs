ea6efec294fa31a96e55ba9da51ef2c6c7b14f63
Peace Thabiwa <peacethabibinflow@proton.me>
2025-11-27 02:42:47 +0100

    Create ThemePhysicsProfile.cs

---
+++ src/DimensionalUI.Core/Themes/ThemePhysicsProfile.cs
@@ -0,0 +1,8 @@
+namespace DimensionalUI.Core.Themes;
+
+public class ThemePhysicsProfile
+{
+    public double Inertia { get; init; } = 1.0;
+    public double Damping { get; init; } = 0.9;
+    public double WobbleAmplitude { get; init; } = 1.0;
+}
